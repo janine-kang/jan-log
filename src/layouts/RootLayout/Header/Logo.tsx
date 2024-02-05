d0c60604ecc7b803490513ee1256573eafe06dd8
janine-kang <developerjanine@gmail.com>
2024-02-06 01:13:15 +0900

    update: modify css

--- src/layouts/RootLayout/Header/Logo.tsx
+++ src/layouts/RootLayout/Header/Logo.tsx
@@ -16,5 +16,8 @@ const StyledWrapper = styled(Link)`
   h2 {
     padding: 0.5rem 0;
     font-weight: 700;
+    background: -webkit-linear-gradient(300deg, #b4d9e7 0%, #93c4d5 10%, #71aec3 20%, #4f98b2 30%, #2d82a0 40%, #0b6c8e 50%, #095b7f 60%, #084a70 70%, #063960 80%, #042851 90%, #021741 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
   }
 `
