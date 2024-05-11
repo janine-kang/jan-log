d972983f33d3cbcfeacc3087683f1abb2ba7018f
janine-dev <developerjanine@gmail.com>
2024-05-11 23:50:11 +0900

    modify: section data type

--- src/libs/utils/index.ts
+++ src/libs/utils/index.ts
@@ -1,3 +1,6 @@
+import { colors } from "src/styles"
+import { TSection } from "src/types"
+
 export function formatDate(date: any, local: any = "en-US") {
   const d = new Date(date)
   const options: any = {
@@ -11,4 +14,34 @@ export function formatDate(date: any, local: any = "en-US") {
 
 export function capitalizeFirstLetter(word: string) {
   return word.charAt(0).toUpperCase() + word.slice(1)
+}
+
+export function toTSection(key: string): TSection {
+  switch (key) {
+    case "archive":
+      return TSection.archive
+    case "books":
+      return TSection.books
+    case "journal":
+      return TSection.journal
+    case "work":
+      return TSection.work
+    default:
+      return TSection.none
+  }
+}
+
+export function getSectionColor(section: TSection): string {
+  switch (section) {
+    case TSection.archive:
+      return colors.light.indigo6
+    case TSection.books:
+      return colors.light.pink5
+    case TSection.journal:
+      return colors.light.yellow5
+    case TSection.work:
+      return colors.light.green6
+    default:
+      return colors.light.red10
+  }
 }
