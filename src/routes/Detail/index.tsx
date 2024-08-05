e8ef70d5f5f17123d5c12a1b605fca9a3a236066
janine-dev <developerjanine@gmail.com>
2024-08-05 18:39:06 +0900

    fix: remove library mermaid

--- src/routes/Detail/index.tsx
+++ src/routes/Detail/index.tsx
@@ -1,13 +1,9 @@
-import useMermaidEffect from "./hooks/useMermaidEffect"
 import PostDetail from "./PostDetail"
 import styled from "@emotion/styled"
-import usePostQuery from "src/general/hooks/usePostQuery"
 
 type Props = {}
 
 const Detail: React.FC<Props> = () => {
-  useMermaidEffect()
-
   return (
     <StyledWrapper>
       <PostDetail />
