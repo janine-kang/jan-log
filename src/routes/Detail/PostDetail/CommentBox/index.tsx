840ce27ccf79a8e77944acf4f9733baae3715597
janine-dev <developerjanine@gmail.com>
2024-08-26 14:06:54 +0900

    feature: add giscus

--- src/routes/Detail/PostDetail/CommentBox/index.tsx
+++ src/routes/Detail/PostDetail/CommentBox/index.tsx
@@ -1,33 +1,19 @@
-import { TPost } from "src/types"
 import { CONFIG } from "site.config"
 import dynamic from "next/dynamic"
 
-const UtterancesComponent = dynamic(
+const GiscusComponent = dynamic(
   () => {
-    return import("./Utterances")
-  },
-  { ssr: false }
-)
-const CusdisComponent = dynamic(
-  () => {
-    return import("./Cusdis")
+    return import("./GiscusComment")
   },
   { ssr: false }
 )
 
 type Props = {
-  data: TPost
+  // data: TPost
 }
 
-const CommentBox: React.FC<Props> = ({ data }) => {
-  return (
-    <div>
-      {CONFIG.utterances.enable && <UtterancesComponent issueTerm={data.id} />}
-      {CONFIG.cusdis.enable && (
-        <CusdisComponent id={data.id} slug={data.slug} title={data.title} />
-      )}
-    </div>
-  )
+const CommentBox: React.FC<Props> = () => {
+  return <div>{CONFIG.giscus.enable && <GiscusComponent />}</div>
 }
 
 export default CommentBox
