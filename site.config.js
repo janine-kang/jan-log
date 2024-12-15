c047d9582651d46463ab5e47fd2de2bd2372c822
janine-dev <developerjanine@gmail.com>
2024-12-15 22:05:59 +0900

    fix: config

--- site.config.js
+++ site.config.js
@@ -10,7 +10,7 @@ const CONFIG = {
     about:
       "Front-end 부터 iOS Native 개발까지 가능한 \n 클라이언트 개발자 강재닌 입니다. \n\n 주어진 것을 반드시 해냅니다. \n 성장하는 것을 즐깁니다.",
     email: "developerjanine@gmail.com",
-    linkedin: "https://www.linkedin.com/in/janine-kang/",
+    linkedin: "janine-kang",
     github: "janine-kang",
     instagram: "",
   },
